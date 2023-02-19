3eb40d933d55e2143f131586fe8e4a90a205f17a
GabrielTeodoroSan <gabrielteodorodomingues@gmail.com>
2023-02-19 19:01:32 -0300

    adding category field from galery models.

--- galery/models.py
+++ galery/models.py
@@ -3,9 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Photographs(models.Model):
+
+    category_choices = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta")
+    ]
+
     name = models.CharField(max_length=30, null=False, blank=False)
     legend = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
+    category = models.CharField(max_length=20, choices=category_choices, default="")
     photo = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
